9d7f4db376304df8ff41feee31f122c8e7301221
alexandredeleze <alexandre.deleze@epfl.ch>
2020-10-20 23:04:56 +0200

    Add private keys and certificates for all components for HTTPS communication

--- CA/setup.sh
+++ CA/setup.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 echo "Nothing to be set up..."
 ifconfig enp0s3 10.0.20.20 netmask 255.255.255.0 up
+apt-get install -y openssl
+mkdir /etc/ssl/CA
+mkdir /etc/ssl/CA/certs
+mkdir /etc/ssl/CA/newcerts
+mkdir /etc/ssl/CA/private
+bash -c "echo '01' > /etc/ssl/CA/serial"
+touch /etc/ssl/CA/index.txt
+cp /media/asl/CA/openssl.cnf /etc/ssl/
+cp /media/asl/CA/cakey.pem /etc/ssl/CA/private
+cp /media/asl/CA/cacert.pem /etc/ssl/CA
+cp /media/asl/CA/.rnd /home/ubuntu
