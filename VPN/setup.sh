150c24c6b3e5f8aef460e6146f34ffb1b4e4cab3
alexandredeleze <alexandre.deleze@epfl.ch>
2020-10-26 14:34:13 +0100

    Add VPN functionality from Client to all servers for ssh

--- VPN/setup.sh
+++ VPN/setup.sh
@@ -2,3 +2,16 @@
 echo "Nothing to be set up..."
 ifconfig enp0s3 192.168.1.30 netmask 255.255.255.0 up
 ip route add 10.0.20.0/24 via 192.168.1.40
+
+# Create VPN connection with wireguard
+cp /media/asl/VPN/wg_private /home/ubuntu/.ssh/
+ip link add wg0 type wireguard
+ip addr add 10.0.0.30/24 dev wg0
+wg set wg0 listen-port 39198 private-key /home/ubuntu/.ssh/wg_private
+ip link set wg0 up
+wg set wg0 peer 6ejIgrueSPwWZ9ZFtb0kckmWqHBRctP1fVCb4T6W4yk= allowed-ips 10.0.0.10 endpoint 192.168.1.10:55027
+# NAT connection coming from VPN
+sysctl -w net.ipv4.ip_forward=1
+iptables -A FORWARD -i wg0 -j ACCEPT
+iptables -A FORWARD -o wg0 -j ACCEPT
+iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE
