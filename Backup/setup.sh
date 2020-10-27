24e4ff98f242b4203b6e4623d1e2363147c3bd92
Manuel Meinen <mmeinen@student.ethz.ch>
2020-10-27 09:54:12 +0100

    allow authenticated SFTP connection between backup server and MySQLServer

--- Backup/setup.sh
+++ Backup/setup.sh
@@ -2,3 +2,9 @@
 echo "Nothing to be set up..."
 ifconfig enp0s3 10.0.20.50 netmask 255.255.255.0 up
 ip route add 192.168.1.0/24 via 10.0.20.40
+
+# Copy SSH keys to the right directory
+cp /media/asl/Backup/backup_sftp_key /home/ubuntu/.ssh/
+cp /media/asl/Backup/backup_sftp_key.pub /home/ubuntu/.ssh/
+cp /media/asl/Backup/known_hosts /home/ubuntu/.ssh/
+chmod 755 /home/ubuntu/.ssh/backup_sftp_key
--- MySQLDatabase/setup.sh
+++ MySQLDatabase/setup.sh
@@ -2,3 +2,18 @@
 echo "Nothing to be set up..."
 ifconfig enp0s3 10.0.20.30 netmask 255.255.255.0 up
 ip route add 192.168.1.0/24 via 10.0.20.40
+# Adding a backup_user
+username="backup_user"
+password="ubuntu" #TODO: change the password
+pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+useradd -m -p "$pass" "$username"
+adduser "$username" sudo
+# Create Backup Directory
+mkdir "backup_dir"
+chown "backup_user" "backup_dir"
+chmod 0703 "backup_dir"
+#SFTP keys for login without password
+mkdir /home/backup_user/.ssh
+chmod 755 /home/backup_user/.ssh
+cp /media/asl/MySQLDatabase/authorized_keys /home/backup_user/.ssh
+chmod 755 /home/backup_user/.ssh/authorized_keys
