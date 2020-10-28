88a9d460c85f8b02a7d7236eab042b9e0b50684e
Manuel Meinen <mmeinen@student.ethz.ch>
2020-10-28 08:35:32 +0100

    set up sftp connection to core server

--- Core/setup.sh
+++ Core/setup.sh
@@ -4,3 +4,19 @@ ifconfig enp0s3 10.0.20.10 netmask 255.255.255.0 up
 ifconfig enp0s8 10.0.10.10 netmask 255.255.255.0 up
 ip route add 192.168.1.0/24 via 10.0.10.40
 ip route add 192.168.1.30/32 via 10.0.20.40
+
+# Adding a backup_user
+username="backup_user"
+password="ubuntu" #TODO: change the password
+pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+useradd -m -p "$pass" "$username"
+adduser "$username" sudo
+# Create Backup Directory
+mkdir "backup_dir"
+chown "backup_user" "backup_dir"
+chmod 0703 "backup_dir"
+#SFTP keys for login without password
+mkdir /home/backup_user/.ssh
+chmod 755 /home/backup_user/.ssh
+cp /media/asl/Core/authorized_keys /home/backup_user/.ssh
+chmod 755 /home/backup_user/.ssh/authorized_keys
