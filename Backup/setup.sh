776f4dc23cb4aea5cf436bc9ef6c6479c0d8ba55
Manuel Meinen <mmeinen@student.ethz.ch>
2020-11-05 19:40:53 +0100

    BackupServer Codebase copied to home

--- Backup/setup.sh
+++ Backup/setup.sh
@@ -1,30 +1,9 @@
 #!/bin/bash
-echo "Nothing to be set up..."
+echo "Setup script started..."
 ifconfig enp0s3 10.0.20.50 netmask 255.255.255.0 up
 ip route add 192.168.1.0/24 via 10.0.20.40
 
-# Copy SSH keys to the right directory
-cp /media/asl/Backup/backup_sftp_key /home/ubuntu/.ssh/
-cp /media/asl/Backup/backup_sftp_key.pub /home/ubuntu/.ssh/
-cp /media/asl/Backup/known_hosts /home/ubuntu/.ssh/
-chmod 755 /home/ubuntu/.ssh/backup_sftp_key
+# Do the stuff that only needs to be done when installing
+./installation.sh
 
-# Install pysftp and python3
-apt-get update
-apt-get upgrade -y
-apt-get -y install python3 python3-pip zip
-python3 -m pip install pysftp
 
-# Setup backup directories
-mkdir ~/backup_MySQLDatabase
-chmod 777 ~/backup_MySQLDatabase
-mkdir ~/backup_Core
-chmod 777 ~/backup_Core
-mkdir ~/backup_CA
-chmod 777 ~/backup_CA
-mkdir ~/backup_Firewall
-chmod 777 ~/backup_Firewall
-mkdir ~/backup_WebServer
-chmod 777 ~/backup_WebServer
-mkdir ~/backup_VPN
-chmod 777 ~/backup_VPN
