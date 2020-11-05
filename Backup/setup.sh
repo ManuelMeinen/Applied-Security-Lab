cb9e38774a2db2695ced7b2dbba6e357647b80a3
Manuel Meinen <mmeinen@student.ethz.ch>
2020-11-05 22:56:25 +0100

    Crontab bug fixed

--- Backup/setup.sh
+++ Backup/setup.sh
@@ -4,6 +4,6 @@ ifconfig enp0s3 10.0.20.50 netmask 255.255.255.0 up
 ip route add 192.168.1.0/24 via 10.0.20.40
 
 # Do the stuff that only needs to be done when installing
-./installation.sh
+/media/asl/Backup/installation.sh
 
 
