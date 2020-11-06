fa314e7fd85e50989a692865cee57c6a359fcc47
Valentyna-Pavliv <valentyna.pavliv@epfl.ch>
2020-11-06 19:51:26 +0100

    preparation sql file

--- MySQLDatabase/setup.sh
+++ MySQLDatabase/setup.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
-echo "Nothing to be set up..."
+echo "sqlServer setup"
 ifconfig enp0s3 10.0.20.30 netmask 255.255.255.0 up
 ip route add 192.168.1.0/24 via 10.0.20.40
 # Adding a backup_user
-username="backup_user"
-password="ubuntu" #TODO: change the password
-pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-useradd -m -p "$pass" "$username"
-adduser "$username" sudo
+#username="backup_user"
+#password="ubuntu" #TODO: change the password
+#pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+#useradd -m -p "$pass" "$username"
+#adduser "$username" sudo
+
+
+#install mysql
+echo "INSTALLATION OF MYSQL"
+#sudo apt update
+#sudo apt install mysql-server
+sudo mysql -u root < imovies.sql
+
+
+
+echo "MYSQL INSTALLED"
+
+
 # Create Backup Directory
-mkdir "backup_dir"
-chown "backup_user" "backup_dir"
-chmod 0703 "backup_dir"
+#mkdir "backup_dir"
+#chown "backup_user" "backup_dir"
+#chmod 0703 "backup_dir"
 #SFTP keys for login without password
-mkdir /home/backup_user/.ssh
-chmod 755 /home/backup_user/.ssh
-cp /media/asl/MySQLDatabase/authorized_keys /home/backup_user/.ssh
-chmod 755 /home/backup_user/.ssh/authorized_keys
+#mkdir /home/backup_user/.ssh
+#chmod 755 /home/backup_user/.ssh
+#cp /media/asl/MySQLDatabase/authorized_keys /home/backup_user/.ssh
+#chmod 755 /home/backup_user/.ssh/authorized_keys
+
