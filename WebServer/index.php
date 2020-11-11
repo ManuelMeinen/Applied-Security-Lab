75c31cc3449e7e1997ee2e7bc994edbb2826eb58
Yann gabbud <yann.gabbud@epfl.ch>
2020-11-11 22:03:40 +0100

    working user session!

--- WebServer/index.php
+++ WebServer/index.php
@@ -3,18 +3,18 @@
 
   // If user already logged in, send them to index
   if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
-      header('Location:login.php');
-      exit;
+    header('Location:login.php');
+    exit;
   }
 ?>
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>iMovie</title>
-    </head>
-    <body>
-        <h2>iMovie</h2>
-        <p>Welcome to the iMovie website!</p>
-    </body>
+  <head>
+      <title>iMovie</title>
+  </head>
+  <body>
+    <h2>iMovie</h2>
+    <p>Welcome to the iMovie website!</p>
+  </body>
 </html>
--- WebServer/login.php
+++ WebServer/login.php
@@ -3,17 +3,58 @@
 
   // If user already logged in, send them to index
   if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-      header('Location:index.php?');
-      exit;
+    header('Location:index.php');
+    exit;
+  }
+
+  $username = "";
+  $password = "";
+  $error = "";
+  
+  // Processing form
+  if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $username = trim($_POST["username"]);
+    $password = trim($_POST["password"]);
+  
+    if(empty($error)){
+      if($username == 'bob' && $password == 'bob') {
+        session_start();
+                    
+        $_SESSION["loggedin"] = true;
+        $_SESSION["id"] = $id;
+        $_SESSION["username"] = $username;                            
+        
+        header("location: index.php");
+      } else {
+        $error = "Invalid credentials.";
+      }
+    }
   }
 ?>
 
+<!DOCTYPE html>
 <html>
-    <head>
-        <title>login</title>
-    </head>
-    <body>
-        <h2>Login</h2>
-        <p>Please login!</p>
-    </body>
+<head>
+  <title>Login</title>
+</head>
+<body>
+  <div class="wrapper">
+    <h2>Login</h2>
+    <p>Please fill in your credentials to login.</p>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+      <div class="form-group">
+        <label>Username</label>
+        <input type="text" name="username" class="form-control">
+      </div>    
+      <div class="form-group">
+        <label>Password</label>
+        <input type="password" name="password" class="form-control">
+      </div>
+      <div class="form-group">
+        <input type="submit" class="btn btn-lg btn-primary" value="Login">
+      </div>
+      <p><?php echo $error; ?></p>
+    </form>
+  </div>    
+</body>
 </html>
