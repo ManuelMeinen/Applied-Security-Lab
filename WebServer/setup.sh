00ebf94734f798fe373377598d3e519023ab7198
Manuel Meinen <mmeinen@student.ethz.ch>
2020-11-12 09:58:27 +0100

    don't comment out installation stuff --> is needed if you run it for the first time

--- WebServer/setup.sh
+++ WebServer/setup.sh
@@ -25,29 +25,12 @@ mkdir /home/backup_user/.ssh
 chmod 755 /home/backup_user/.ssh
 cp /media/asl/WebServer/authorized_keys /home/backup_user/.ssh
 chmod 755 /home/backup_user/.ssh/authorized_keys
-# # Adding a backup_user
-# username="backup_user"
-# password="ubuntu" #TODO: change the password
-# pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-# useradd -m -p "$pass" "$username"
-# adduser "$username" sudo
-# # Create Backup Directory
-# mkdir "backup_dir"
-# chown "backup_user" "backup_dir"
-# chmod 0703 "backup_dir"
-# #SFTP keys for login without password
-# mkdir /home/backup_user/.ssh
-# chmod 755 /home/backup_user/.ssh
-# cp /media/asl/WebServer/authorized_keys /home/backup_user/.ssh
-# chmod 755 /home/backup_user/.ssh/authorized_keys
 
-
-
-
-# apt update 
-# apt upgrade -y
-# tasksel install lamp-server
-# apt install php libapache2-mod-php
+# Install stuff...
+apt update 
+apt upgrade -y
+tasksel install lamp-server
+apt install php libapache2-mod-php
 
 cp /media/asl/WebServer/000-default.conf /etc/apache2/sites-available/000-default.conf
 cp /media/asl/WebServer/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf
